8ba497023d37c000dcfb6e8d51263bd8363b72af
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-07-12 22:31:56 +0300

    microtask 03.1.1 - button

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,24 @@ function App() {
     //     console.log('Hello Im Ivan!')
     // }
 
-    const onClickHandler=(name:string)=>{
-        console.log(name)
+    // const onClickHandler=(name:string)=>{
+    //     console.log(name)
+    // }
+
+    const foo1=()=> {
+        console.log(100200)
+    }
+    const foo2=(num: number)=>{
+        console.log(num)
     }
 
     return(
         <div>
-            <button onClick={() => onClickHandler("Vasya")}>MyYouTubeChanel-1</button>
-            <button onClick={() => onClickHandler("Ivan")}>MyYouTubeChanel-2</button>
+            {/*<button onClick={() => onClickHandler("Vasya")}>MyYouTubeChanel-1</button>*/}
+            {/*<button onClick={() => onClickHandler("Ivan")}>MyYouTubeChanel-2</button>*/}
+
+            <button onClick={foo1}>1</button>
+            <button onClick={() => foo2(100500)}>2</button>
         </div>
 
     )
