4016b2d4bf6a886a71866bc6727584d290d32652
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-07-13 14:24:58 +0300

    microtask 03.2- button

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
 import React from 'react';
-
+import {Button} from "./Components/Button";
 
 
 function App() {
-    // const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-    //     console.log('Hello Im Vasya!')
-    // }
-    // const mySecondSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-    //     console.log('Hello Im Ivan!')
-    // }
-
-    // const onClickHandler=(name:string)=>{
-    //     console.log(name)
-    // }
-
-    const foo1=()=> {
-        console.log(100200)
+    const Button1Foo=(subscriber: string, age: number, address: string)=>{
+        console.log(subscriber, age, address)
     }
-    const foo2=(num: number)=>{
-        console.log(num)
+    const Button2Foo=(subscriber: string, age: number)=>{
+        console.log(subscriber, age)
     }
-
-    return(
+    const Button3Foo=(subscriber: string)=>{
+        console.log(subscriber)
+    }
+    return (
         <div>
-            {/*<button onClick={() => onClickHandler("Vasya")}>MyYouTubeChanel-1</button>*/}
-            {/*<button onClick={() => onClickHandler("Ivan")}>MyYouTubeChanel-2</button>*/}
-
-            <button onClick={foo1}>1</button>
-            <button onClick={() => foo2(100500)}>2</button>
+            <Button name={'MyYouTubeChanel-1'} callBack={() => Button1Foo('im Vasya', 21, 'Minsk')}/>
+            <Button name={'MyYouTubeChanel-2'} callBack={() => Button2Foo('im Ivan', 35)}/>
+            <Button name={'Stupid button'} callBack={() => Button3Foo('im stupid button')}/>
         </div>
 
     )
