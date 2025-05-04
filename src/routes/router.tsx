70391516666a39465149982e598f4962bc60b88c
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2025-05-05 01:03:43 +0300

    microtasks 2.0/13: Protected Route deep refactoring - 2

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,49 +1,144 @@
 import * as React from 'react';
-import {createBrowserRouter,} from 'react-router-dom';
+import {createBrowserRouter, Navigate, RouteObject,} from 'react-router-dom';
 import App from '../App';
 import {Abibas} from '../components/pages/Abibas';
 import {Adidas} from '../components/pages/Adidas';
 import {Error404} from '../components/pages/Error404';
+import {Login} from '../components/pages/Login';
 import {Model} from '../components/pages/Model';
 import {Prices} from '../components/pages/Prices';
+import {ProtectedPage} from '../components/pages/ProtectedPage';
 import {Puma} from '../components/pages/Puma';
+import {ProtectedRoute} from './ProtectedRoute';
 
 const PATH = {
     ADIDAS: '/adidas',
     PUMA: '/puma',
     ABIBAS: '/abibas',
     PRICES: '/prices',
-    MODEL: '/:model/:id'
+    MODEL: '/:model/:id',
+    ERROR: '/error',
+    PROTECTED: '/protected',
+    LOGIN: '/login'
 } as const
+
+const publicRoutes: RouteObject[] = [
+    {
+        path: PATH.ADIDAS,
+        element: <Adidas/>
+    },
+    {
+        path: PATH.PUMA,
+        element: <Puma/>
+    },
+    {
+        path: PATH.ABIBAS,
+        element: <Abibas/>
+    },
+    {
+        path: PATH.PRICES,
+        element: <Prices/>
+    },
+    {
+        path: PATH.MODEL,
+        element: <Model/>
+    },
+    {
+        path: PATH.ERROR,
+        element: <Error404/>
+    },
+    {
+        path: PATH.LOGIN,
+        element: <Login/>
+    },
+]
+const privateRoutes: RouteObject[] = [
+    {
+        path: PATH.PROTECTED,
+        element:
+            <ProtectedRoute>
+                <ProtectedPage/>
+            </ProtectedRoute>
+    },
+]
+
+export const PrivateRoute = () => {
+    const isAuth = false
+    return <>{isAuth ? <ProtectedPage/> : <Navigate to={'/login'}/>}</>
+};
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <App/>,
-        errorElement: <Error404/>,
+        errorElement: <Navigate to={PATH.ERROR}/>,
         children: [
+            ...publicRoutes,
             {
-                path: PATH.ADIDAS,
-                element: <Adidas/>
-            },
-            {
-                path: PATH.PUMA,
-                element: <Puma/>
-            },
-            {
-                path: PATH.ABIBAS,
-                element: <Abibas/>
-            },
-            {
-                path: PATH.PRICES,
-                element: <Prices/>
-            },
-            {
-                path: PATH.MODEL,
-                element: <Model/>
+                element: <PrivateRoute/>,
+                children: privateRoutes
             }
         ]
     },
 
 ]);
 
+
+//-------------------------
+
+//const PATH = {
+//     ADIDAS: '/adidas',
+//     PUMA: '/puma',
+//     ABIBAS: '/abibas',
+//     PRICES: '/prices',
+//     MODEL: '/:model/:id',
+//     ERROR: '/error',
+//     PROTECTED: '/protected',
+//     LOGIN: '/login'
+// } as const
+//
+// export const router = createBrowserRouter([
+//     {
+//         path: '/',
+//         element: <App/>,
+//         errorElement: <Navigate to={'/error'}/>,
+//         children: [
+//             {
+//                 path: PATH.ADIDAS,
+//                 element: <Adidas/>
+//             },
+//             {
+//                 path: PATH.PUMA,
+//                 element: <Puma/>
+//             },
+//             {
+//                 path: PATH.ABIBAS,
+//                 element: <Abibas/>
+//             },
+//             {
+//                 path: PATH.PRICES,
+//                 element: <Prices/>
+//             },
+//             {
+//                 path: PATH.MODEL,
+//                 element: <Model/>
+//             },
+//             {
+//                 path: PATH.PROTECTED,
+//                 element:
+//                 <ProtectedRoute>
+//                     <ProtectedPage/>
+//                 </ProtectedRoute>
+//             },
+//             {
+//                 path: PATH.ERROR,
+//                 element: <Error404/>
+//             },
+//             {
+//                 path: PATH.LOGIN,
+//                 element: <Login/>
+//             },
+//         ]
+//     },
+//
+// ]);
